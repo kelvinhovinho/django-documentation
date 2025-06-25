2526ac289d6353214935335612323afeac88dabd
kelvinhovinho <kelvinadamba@hotmail.com>
2025-06-26 00:22:32 +0300

    Changed fro function view to class based view

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns =[
-    path("", views.index, name="Home"),
-    path("<int:question_id>/", views.details, name="Detail"),
-    path("<int:question_id>/results/", views.results, name="result"),
+    path("", views.IndexView.as_view(), name="Home"),
+    path("<int:pk>/", views.DetailView.as_view(), name="Detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="result"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
