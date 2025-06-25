bde18bc29e1ba93acd833be02de2b716da6c1342
kelvinhovinho <kelvinadamba@hotmail.com>
2025-06-25 14:34:10 +0300

    .

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns =[
-    path("", views.index, name="Home")
+    path("", views.index, name="Home"),
+    path("<int:question_id>/", views.details, name="Detail"),
+    path("<int:question_id>/results/", views.results, name="result"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
